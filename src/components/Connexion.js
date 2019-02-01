734e73b0d84334f84d30e361b08c5200602e6c8d
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-01 10:45:14 +0100

    add redirect url pseudo

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
 class Connexion extends Component {
+
+    state = {
+        pseudo: '',
+        goToChat: false
+    }
+
+    handleChange = event => {
+        const pseudo = event.target.value
+        this.setState({ pseudo })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.setState({ goToChat: true })
+    }
+
     render () {
+
+        if (this.state.goToChat) {
+            return <Redirect push to={`/pseudo/${this.state.pseudo}`} />
+        }
+
         return (
             <div className='connexionBox'>
-                <form className='connexion'>
-                    <input 
+                <form className='connexion' onSubmit={this.handleSubmit}>
+                    <input
+                        value={this.state.pseudo}
+                        onChange={this.handleChange}
                         placeholder='Pseudo'
-                        type='text' 
+                        type='text'
                         required 
                     />
                     <button type='submit'>GO</button>
