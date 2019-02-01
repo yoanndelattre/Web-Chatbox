b82aa68308177f8886f6602194195139000b0541
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-01 13:41:37 +0100

    create Formulaire Message

---
+++ src/components/Formulaire.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+class Formulaire extends Component {
+
+    state = {
+        message: ''
+    }
+
+    createMessage = () => {
+        const { addMessage, pseudo } = this.props
+
+        const message = {
+            pseudo,
+            message: this.state.message
+        }
+
+        addMessage(message)
+
+        //Reset
+        this.setState({ message: '' })
+    }
+    
+    handleSubmit = event => {
+        event.preventDefault()
+        this.createMessage()
+    }
+
+    handleChange = event => {
+        const message = event.target.value
+        this.setState ({ message })
+    }
+
+    render () {
+      return (
+        <form className='form' onSubmit={this.handleSubmit}>
+            <textarea value={this.state.message} onChange={this.handleChange} required maxLength={this.props.length} />
+            <div className='info' >
+                140
+            </div>
+            <button type='submit' >
+                Envoyer
+            </button>
+      </form>
+      ) 
+    }
+}
+
+export default Formulaire
