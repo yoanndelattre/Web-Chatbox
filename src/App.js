34e8ffe22da5f56c5112a5e6b5c354e5ba8a7bc3
yoanndelattre <yoanndelattre21@gmail.com>
2019-02-06 13:06:22 +0100

    add sync database firebase

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import './App.css'
 import Formulaire from './components/Formulaire'
 import Message from './components/Message'
 
+//Firebase
+import base from './conf-firebase'
+
 class App extends Component {
 
   state = {
     messages: {},
     pseudo: this.props.match.params.pseudo
+  }
+
+  componentDidMount () {
+    base.syncState('/', {
+      context: this,
+      state: 'messages'
+    })
   }
 
   addMessage = message => {
