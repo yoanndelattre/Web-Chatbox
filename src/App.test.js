3c7307eaad1a27d4153924d288d827337dee2fcf
yoanndelattre <yoanndelattre21@gmail.com>
2020-08-30 11:54:34 +0200

    setup new strategy test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Routes
+import { render } from '@testing-library/react';
 import MainRouter from './Routes'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainRouter/>, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders Home Page', () => {
+  const { getByText } = render(<MainRouter/>);
+  const linkElement = getByText(/Chatbox - Conversation/i);
+  expect(linkElement)
 });
