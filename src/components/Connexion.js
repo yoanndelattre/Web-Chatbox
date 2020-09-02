40497895499f34beaff8c73a722b3099f400848b
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-02 10:23:10 +0200

    remove old auth method

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -9,16 +9,6 @@ class Connexion extends Component {
         goToChat: false
     }
 
-    handleChange = event => {
-        const pseudo = event.target.value
-        this.setState({ pseudo })
-    }
-
-    handleSubmit = event => {
-        event.preventDefault()
-        this.setState({ goToChat: true })
-    }
-
     render () {
 
         if (this.state.goToChat) {
@@ -32,18 +22,7 @@ class Connexion extends Component {
                     <h1 className="title">Chatbox - Conversation</h1>
                     <div className='connexionBox'>
                         <a className='report-problem' target="blank" href="https://github.com/yoanndelattre/Web-Chatbox/issues">Report a Problem</a>
-                        <div className="border">
-                            <form className='connexion' onSubmit={this.handleSubmit}>
-                                <input
-                                    value={this.state.pseudo}
-                                    onChange={this.handleChange}
-                                    placeholder='Pseudo'
-                                    type='text'
-                                    required 
-                                />
-                                <button type='submit'>GO</button>
-                            </form>
-                        </div>
+                        
                     </div>
                 </div>
             </Fragment>
