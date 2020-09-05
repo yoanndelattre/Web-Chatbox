81f6cdcaa06f01bcd4e09deb28eea1d95efa7d4a
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-05 22:35:30 +0200

    auto fix eslint error syntax

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react'
-import CookieAlert from '../Cookie-Alert'
+import React, {Fragment} from 'react';
+import CookieAlert from '../Cookie-Alert';
 
 const NotFound = () => (
-    <Fragment>
-        <CookieAlert/>
-        <h2 className='notFound'>404 NotFound</h2>
-        <a className='report-problem' href="mailto:contact@yoanndelattre.com">Report a Problem</a>
-    </Fragment>
-)
+  <Fragment>
+    <CookieAlert/>
+    <h2 className='notFound'>404 NotFound</h2>
+    <a className='report-problem' href="mailto:contact@yoanndelattre.com">Report a Problem</a>
+  </Fragment>
+);
 
-export default NotFound
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 // Routes
-import MainRouter from './Routes'
+import MainRouter from './Routes';
 
 ReactDOM.render(<MainRouter/>, document.getElementById('root'));
 
