6690d03a81ff0f08349d5ef549437ff2b91d5de5
yoanndelattre <yoanndelattre21@gmail.com>
2020-09-05 22:49:43 +0200

    eslint fix max-len in NotFound.js file

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,7 +5,9 @@ const NotFound = () => (
   <Fragment>
     <CookieAlert/>
     <h2 className='notFound'>404 NotFound</h2>
-    <a className='report-problem' href="mailto:contact@yoanndelattre.com">Report a Problem</a>
+    <a className='report-problem' href="mailto:contact@yoanndelattre.com">
+      Report a Problem
+    </a>
   </Fragment>
 );
 
